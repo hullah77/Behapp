54e53ee9e7a488451fd5a2e1005c2892409c8b18
hullah77 <168562740+hullah77@users.noreply.github.com>
2024-04-30 14:33:54 -0400

    Add files via upload

---
+++ behapp_utils.R
@@ -0,0 +1,25 @@
+
+#Load daily data
+
+load.daily.data <- function(daily.dataset) {
+    daily.data <- read.csv(data.frame(dataset))
+    return(daily.data)
+}
+
+
+#Load weekly data
+
+
+#Add demographic data
+
+add.demo <- function(daily.data, demo) {
+
+}
+
+#Create scatterplot of endpoint vs. time
+
+#Data availability chart
+
+
+
+
